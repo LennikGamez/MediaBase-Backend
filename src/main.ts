6f98701e2f6fa8c88184f9bfac4a2c777ce639db
Leander <leander@doerk.org>
2025-07-13 15:12:39 +0200

    library route logic according to the structure plan that is not commited yet

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,58 @@
 import { Application, Request, Response } from "express"; 
 // const express = require("express");
 import express from "express";
+import fs from "node:fs";
+import path from "node:path"; 
 
+const BASE_DIRECTORY = process.env.BASE_DIRECTORY as string;
 
 const app: Application = express();
+
+enum MediaTypes {
+  MOVIE,
+  SHOW,
+  AUDIO,
+  UNDEFINED
+}
+
+type LibEntry = {
+  name: string,
+  type: MediaTypes
+}
+
+function mapStringToMediaType(str: string): MediaTypes{
+  switch(str){
+    case "Filme":
+      return MediaTypes.MOVIE;
+    case "Serien":
+      return MediaTypes.SHOW;
+    case "Hörbücher":
+      return MediaTypes.AUDIO;
+    default:
+      return MediaTypes.UNDEFINED;
+  }
+}
 
 app.get("/library", (req: Request, res: Response) => {
   // [{name: "FilmName", type: "MOVIE"}]
 
-  
+  const lib: LibEntry[] = [];
+  const firstLevel = fs.readdirSync(BASE_DIRECTORY)
+  firstLevel.forEach((dir) => {
+    const joinedPath = path.join(BASE_DIRECTORY, dir);
+    if (!fs.statSync(joinedPath).isDirectory) return;
+    const mediaType = mapStringToMediaType(dir);
+
+    const secondLevel = fs.readdirSync(joinedPath);
+    secondLevel.forEach((media) => {
+      lib.push({
+        name: media,
+        type: mediaType
+      })
+    })
+
+  })
+  res.json(lib);
 })
 
 
