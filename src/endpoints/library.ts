79e7928c0d609fda367fc1847b7d3db5a5bb1369
Leander <leander@doerk.org>
2025-07-17 16:37:51 +0200

    poster path is now also part of the library endpoint response

--- src/endpoints/library.ts
+++ src/endpoints/library.ts
@@ -4,10 +4,12 @@ import path from "node:path";
 
 import { BASE_DIRECTORY } from "../helpers/fileoperations"; 
 import { MediaTypes, mapStringToMediaType } from "../helpers/util"; 
+import { filterFiles } from "../helpers/dirScans";
 
 export type LibEntry = {
   name: string,
-  type: MediaTypes
+  type: MediaTypes,
+  poster: string
 }
 export function registerLibraryEndpoint(appHandle: Application){
   appHandle.get("/library", (_req: Request, res: Response) => {
@@ -22,9 +24,13 @@ export function registerLibraryEndpoint(appHandle: Application){
 
       const secondLevel = fs.readdirSync(joinedPath);
       secondLevel.forEach((media) => {
+        const thirdLevel = fs.readdirSync(path.join(joinedPath, media), {withFileTypes: true});
+        const poster = filterFiles(thirdLevel, [".png", ".jpg"])[0];
+
         lib.push({
           name: media,
-          type: mediaType
+          type: mediaType,
+          poster: path.join(poster.parentPath, poster.name).replace(BASE_DIRECTORY, '')
         })
       })
 
